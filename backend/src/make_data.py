d24e5dc3fa982aecd70d67f3c0d07fb1e72be899
chnk5 <chn.k58.hoan@gmail.com>
2021-12-08 08:44:03 +0700

    change dataset

--- backend/src/make_data.py
+++ backend/src/make_data.py
@@ -8,16 +8,23 @@ import numpy as np
 
 detector=MTCNN()
 landmark_detector=dlib.shape_predictor("../models/shape_predictor_68_face_landmarks.dat")
-data_folder="../face_data"
+data_folder="../FaceShape Dataset/training_set"
 
 landmark_list=[]
 label_list=[]
 
 for label in os.listdir(data_folder):
+    if '.' in label:
+        continue
+    cnt=0
     path_label=os.path.join(data_folder,label)
     for file_name in os.listdir(path_label):
+        if cnt>=200:
+            break
         path_file=os.path.join(path_label,file_name)
         image=cv2.imread(path_file)
+        if image is None:
+            continue
         results=detector.detect_faces(image)
         if len(results)>0:
             result = results[0]
@@ -31,6 +38,7 @@ for label in os.listdir(data_folder):
             landmark=landmark.reshape(68*2)
             landmark_list.append(landmark)
             label_list.append(label)
+            cnt+=1
 
             
 print(len(landmark_list))
--- backend/src/train_model.py
+++ backend/src/train_model.py
@@ -12,16 +12,6 @@ print(len(label_list))
 
 model=svm.SVC(kernel="linear")
 model.fit(landmark_list,label_list)
-cnt=0
-total=len(label_list)
-for x in range(total):
-    result= model.predict([landmark_list[x]])
-    if(result[0]==label_list[x]):
-        cnt+=1
-    else:
-        print(x,result[0],label_list[x])
-
-print(cnt/total)
 
 file=open("../models/model.sav",'wb')
 pickle.dump(model,file)
