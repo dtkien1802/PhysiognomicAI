f53494c2db413f5663a9c339cdd00cb3728be186
chnk5 <chn.k58.hoan@gmail.com>
2021-12-17 22:04:21 +0700

    them tinh nang chup anh

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,14 +1,14 @@
 import pickle
-from flask import Flask, render_template, request
+from typing import Sequence
+from flask import Flask, render_template, request,jsonify
 import dlib
-import os
 from mtcnn.mtcnn import MTCNN
 from random import random
 import cv2
 from imutils import face_utils
+import base64
+import numpy as np
 
-
-# Khởi tạo Flask~
 app = Flask(__name__, template_folder="../../frontend")
 app.config['UPLOAD_FOLDER'] = "static"
 
@@ -27,77 +27,56 @@ detector = MTCNN()
 predictor = dlib.shape_predictor(
     "../models/shape_predictor_68_face_landmarks.dat")
 
-
-# Hàm xử lý request
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET'])
 def home_page():
-    # Nếu là POST (gửi file)
-    if request.method == "POST":
-        try:
-            # Lấy file gửi lên
-            image = request.files['file']
-            if image:
-                # Lưu file
-                print(image.filename)
-                print(app.config['UPLOAD_FOLDER'])
-                path_to_save = os.path.join(
-                    app.config['UPLOAD_FOLDER'], image.filename)
-                print("Save = ", path_to_save)
-                image.save(path_to_save)
-
-                # Convert image to dest size tensor
-                frame = cv2.imread(path_to_save)
-
-                results = detector.detect_faces(frame)
-
-                if len(results) != 0:
-                    for result in results:
-                        x1, y1, width, height = result['box']
-
-                        x1, y1 = abs(x1), abs(y1)
-                        x2, y2 = x1 + width, y1 + height
-                        face = frame[y1:y2, x1:x2]
-
-                        # Extract dlib
-                        landmark = predictor(
-                            frame, dlib.rectangle(x1, y1, x2, y2))
-                        landmark = face_utils.shape_to_np(landmark)
-
-                        print("O", landmark.shape)
-                        landmark = landmark.reshape(68 * 2)
-                        print("R", landmark.shape)
-
-                        y_pred = clf.predict([landmark])
-                        print(y_pred)
-
-                        extra = dict[y_pred[0]][1]
-                        ID = dict[y_pred[0]][0]
-
-                        cv2.imwrite(path_to_save, face)
-                        break
-
-                    # Trả về kết quả
-                    return render_template("index.html", user_image=image.filename, rand=str(random()),
-                                           msg="Tải file lên thành công", idBoolean=True, ID=ID, extra=extra, hasface=True, face_shape=ID)
-                else:
-                    return render_template('index.html', msg='Không nhận diện được khuôn mặt')
-            else:
-                # Nếu không có file thì yêu cầu tải file
-                return render_template('index.html', msg='Hãy chọn file để tải lên')
-
-        except Exception as ex:
-            # Nếu lỗi thì thông báo
-            print(ex)
-            return render_template('index.html', msg='Không nhận diện được khuôn mặt')
-
-    else:
-        # Nếu là GET thì hiển thị giao diện upload
-        return render_template('index.html')
+    return render_template('index.html')
 
 @app.route("/about", methods=['GET'])
 def about_page():
     return render_template('about.html', team_image="a.jpg")
 
+cnt=1
+@app.route('/', methods=['POST'])
+def AI():
+    try:
+        global cnt
+        img_base64 = request.form["img"]
+        path_to_save='image/img'+str(cnt)+'.jpg'
+        cnt=cnt+1
+        img_binary = base64.b64decode(img_base64)
+        img_jpg=np.frombuffer(img_binary, dtype=np.uint8)
+        img = cv2.imdecode(img_jpg, cv2.IMREAD_COLOR)
+        cv2.imwrite(path_to_save, img)
+        frame = cv2.imread(path_to_save)
+        results = detector.detect_faces(frame)
+
+        if len(results) != 0:
+            for result in results:
+                x1, y1, width, height = result['box']
+                x1, y1 = abs(x1), abs(y1)
+                x2, y2 = x1 + width, y1 + height
+                face = frame[y1:y2, x1:x2]
+                landmark = predictor(
+                    frame, dlib.rectangle(x1, y1, x2, y2))
+                landmark = face_utils.shape_to_np(landmark)
+                landmark = landmark.reshape(68 * 2)
+
+                y_pred = clf.predict([landmark])
+                extra = dict[y_pred[0]][1]
+                ID = dict[y_pred[0]][0]
+
+                cv2.imwrite(path_to_save, face)
+                break
+
+            return jsonify(
+                msg="Gửi ảnh lên server lên thành công",
+                extra=extra,
+                face_shape=ID
+            )
+        else:
+            return jsonify( msg="Không nhận diện được khuôn mặt")
+    except Exception as ex:
+        return jsonify( msg="Không nhận diện được khuôn mặt")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True,ssl_context='adhoc')
+    app.run(debug=True)
---
+++ backend/src/static/main.js
@@ -0,0 +1,74 @@
+let camera_button = document.getElementById("start-camera");
+let video = document.getElementById("video");
+let canvas = document.getElementById("canvas");
+let preview = document.querySelector('img');
+var buttonSubmit=document.getElementById('submit');
+var fileInput=document.getElementById('fileinput')
+
+buttonSubmit.addEventListener("click",function (){
+    camera_button.textContent="Start Camera"
+    if(video.srcObject!=null){
+        video.srcObject.getTracks().forEach(function(track) {
+            track.stop();
+        });
+    }
+
+    const file = fileInput.files[0];
+    const reader = new FileReader();
+    reader.addEventListener("load", function () {
+    preview.src = reader.result;
+    sendImageToServer(reader.result)
+    }, false);
+
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+    img.hidden=false
+    video.hidden=true
+    canvas.hidden=true
+})
+
+camera_button.addEventListener('click', async function() {
+    if(camera_button.textContent=="Start Camera")
+    {
+        camera_button.textContent="Capture Image"
+
+        let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+        video.srcObject = stream;
+        video.hidden=false
+        canvas.hidden=true
+        img.hidden=true
+
+    }else
+    {
+        camera_button.textContent="Start Camera"
+        canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+        let img_base64 = canvas.toDataURL('image/jpeg')
+        sendImageToServer(img_base64)
+        video.srcObject.getTracks().forEach(function(track) {
+            track.stop();
+        });
+        video.hidden=true
+        canvas.hidden=false
+        img.hidden=true
+    }
+
+});
+
+function sendImageToServer(img_base64)
+{
+    img_base64=img_base64.replace(/^.*,/, '');
+    const body = new FormData();
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var x=JSON.parse(xhr.responseText)
+            document.getElementById("result_id").innerHTML=x['msg']
+            document.getElementById("extra").innerHTML=x['extra']
+            document.getElementById("result_id_number").innerHTML=x['face_shape']
+        }
+    };
+    body.append('img', img_base64);
+    xhr.open('POST', '/', true);
+    xhr.send(body);
+}
